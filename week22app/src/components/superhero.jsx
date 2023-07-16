dd9c7bdef61bd5a4465e5fecb0691b9ce4ee1403
grigaskate <grigaskate@gmail.com>
2023-07-16 10:54:46 +0300

    first commit

---
+++ week22app/src/components/superhero.jsx
@@ -0,0 +1,21 @@
+import '../style/superhero.css';
+
+
+
+function Superhero(props) {
+    return (
+    <div className="card-superhero">
+        <h2 className="superhero-name">{props.name}</h2>
+        <p className="superhero-universe">Вселенная: {props.universe}</p>
+        <p className="superhero-alterego">Альтер эго: {props.alterego}</p>
+        <p className="superhero-occupation">Род деятельности: {props.occupation}</p>
+        <p className="superhero-friends">Друзья: {props.friends}</p>
+        <p className="superhero-superpowers">Суперсилы: {props.superpowers}</p>
+        <img className="superhero-url" src ={props.url}/>
+        <p className="superhero-info">{props.info}</p>
+    </div>
+    );
+}
+
+
+export default Superhero;
